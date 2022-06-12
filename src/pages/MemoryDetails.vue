1b15a4d0cc77413ab0dad96631153cee8f9668e8
MatheusAltino <matheusaltino.dev@gmail.com>
2022-06-12 15:49:44 -0300

    :sparkles: Add pages and components of memories

---
+++ src/pages/MemoryDetails.vue
@@ -0,0 +1,30 @@
+<template>
+    <base-layout 
+        :page-title="loadedMemory ? loadedMemory.title : 'Carregando...'"
+        page-default-back-link="/memories"
+    >
+        <h2 v-if="!loadedMemory">Não foi possível encontrar esta lembrança</h2>
+        <h2 v-else>Carregando...</h2>
+    </base-layout>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            memoryId: this.$route.params.id,
+        }
+    },
+    computed: {
+        loadedMemory () {
+            return this.$store.getters.memory(this.memoryId);
+        }
+    },
+    watch: {
+        '$route'(currentRoute) {
+            this.memoryId = currentRoute.params.id;
+        }
+    }
+}
+
+</script>
